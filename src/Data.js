1f0e86805c51f5346e7a4a950719c9d9a1dd9364
fs1g17 <fs1g17@soton.ac.uk>
2024-02-13 18:23:32 +0000

    fix(poop): scoop

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { Timestamp, addDoc, collection, getDocs } from 'firebase/firestore';
 import db from './firebaseConfig';
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
@@ -7,33 +7,42 @@ import { Chart as ChartJS } from "chart.js/auto";
 
 
 function Data() {
+  const [item, setItem] = useState("");
+  const [price, setPrice] = useState("");
+  const [dateDD, setDateDD] = useState("");
+  const [dateMM, setDateMM] = useState("");
+  const [dateYYYY, setDateYYYY] = useState("");
+
   const handlePushData = async () => {
 
     try {
       await addDoc(collection(db, "history"), {
-        key: "value"
+        item: item, 
+        price: price, 
+        date: Timestamp.fromDate(new Date(dateYYYY, dateMM, dateDD))
       });
+
+      setItem("");
+      setPrice("");
+      setDateDD("");
+      setDateMM("");
+      setDateYYYY("");
     } catch(error) {
       console.log(error);
     }
-
-    // // Add data to a collection
-    // db.collection('history').add({
-    //   // Data to push
-    //   key: 'value',
-    //   // Add more fields as needed
-    // })
-    // .then(() => {
-    //   console.log('Data pushed to Firestore successfully');
-    // })
-    // .catch((error) => {
-    //   console.error('Error pushing data to Firestore: ', error);
-    // });
   };
 
   return (
     <div>
       <button onClick={handlePushData}>Push Data to Firestore</button>
+      <div>
+        <input value={item} onChange={e => setItem(e.currentTarget.value)} placeholder='item' />
+        <input value={price} type='number' onChange={e => setPrice(e.currentTarget.value)} placeholder='price' />
+        
+        <input value={dateDD} onChange={e => setDateDD(e.currentTarget.value)} placeholder='day' />
+        <input value={dateMM} onChange={e => setDateMM(e.currentTarget.value)} placeholder='month' />
+        <input value={dateYYYY} onChange={e => setDateYYYY(e.currentTarget.value)} placeholder='year' />
+      </div>
     </div>
   );
 }
